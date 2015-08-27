c283e2107d8fb1ec10ad065a7e7d862e6478b3ac
梁志艳 <liangzhiyan@kyee.com.cn>
2015-08-28 00:59:40 +0800

    完成图片查看

--- www/js/app.js
+++ www/js/app.js
@@ -1,41 +1,62 @@
 angular.module('weddingApp', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: "/home",
-      templateUrl: "templates/home.html"
-    })
-    .state('lection', {
-      url: "/lection",
-      templateUrl: "templates/lection.html"
-    })
-    .state('timelocation', {
-      url: "/timelocation",
-      templateUrl: "templates/timelocation.html"
-    })
-    .state('thanks', {
-      url: "/thanks",
-      templateUrl: "templates/thanks.html"
-    })
-    .state('photos', {
-      url: "/photos",
-      templateUrl: "templates/photos.html"
-    });
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "templates/home.html"
+        })
+        .state('lection', {
+            url: "/lection",
+            templateUrl: "templates/lection.html"
+        })
+        .state('timelocation', {
+            url: "/timelocation",
+            templateUrl: "templates/timelocation.html"
+        })
+        .state('thanks', {
+            url: "/thanks",
+            templateUrl: "templates/thanks.html"
+        })
+        .state('photos', {
+            url: "/photos",
+            templateUrl: "templates/photos.html",
+            controller: 'photoController'
+        });
 
-  $urlRouterProvider.otherwise("/home");
+    $urlRouterProvider.otherwise("/photos");
 })
 
 // I don't know whethe it's useful if I just need mobile web.
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+    .controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
+        $scope.photos = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
+        $scope.onImgClick = function(index) {
+            $scope.currentIndex = index;
+            $ionicModal.fromTemplateUrl('templates/photo.html', {
+                scope: $scope,
+                animation: 'slide-in-up'
+            }).then(function(modal) {
+                $scope.modal = modal;
+                $scope.modal.show();
+            });
+        }
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+
+    }]);
