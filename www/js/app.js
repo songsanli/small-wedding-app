661f84e287347ebd66ca78f4005b0e462d316dde
Samuel Song <samuel.song@sap.com>
2015-09-02 14:29:02 +0800

    继续修复bug。
    1. 修复背景图片初始缩放的问题
    2. 晓雯希望可以取消掉左上角button的效果，取消掉让她看看
    3. 再次加上slider的pager

--- www/js/app.js
+++ www/js/app.js
@@ -28,35 +28,35 @@ angular.module('weddingApp', ['ionic'])
 })
 
 // I don't know whethe it's useful if I just need mobile web.
-.run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if (window.StatusBar) {
-                StatusBar.styleDefault();
-            }
+// .run(function($ionicPlatform) {
+//         $ionicPlatform.ready(function() {
+//             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//             // for form inputs)
+//             if (window.cordova && window.cordova.plugins.Keyboard) {
+//                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//             }
+//             if (window.StatusBar) {
+//                 StatusBar.styleDefault();
+//             }
+//         });
+//  })
+.controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
+    $scope.photos = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
+    $scope.onImgClick = function(index) {
+        $scope.currentIndex = index;
+        $ionicModal.fromTemplateUrl('templates/photo.html', {
+            scope: $scope,
+            animation: 'slide-in-up'
+        }).then(function(modal) {
+            $scope.modal = modal;
+            $scope.modal.show();
         });
-    })
-    .controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
-        $scope.photos = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
-        $scope.onImgClick = function(index) {
-            $scope.currentIndex = index;
-            $ionicModal.fromTemplateUrl('templates/photo.html', {
-                scope: $scope,
-                animation: 'slide-in-up'
-            }).then(function(modal) {
-                $scope.modal = modal;
-                $scope.modal.show();
-            });
-        }
-        $scope.closeModal = function() {
-            $scope.modal.hide();
-        };
-        $scope.$on('$destroy', function() {
-            $scope.modal&&$scope.modal.remove();
-        });
+    }
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    $scope.$on('$destroy', function() {
+        $scope.modal&&$scope.modal.remove();
+    });
 
-    }]);
+}]);
