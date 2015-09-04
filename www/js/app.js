a85af357830904ac233fbe99a10e914ae9f33a90
Samuel Song <songsanli3@gmail.com>
2015-09-04 21:32:44 +0800

    Add loading indicator.

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,8 @@ angular.module('weddingApp', ['ionic'])
     $stateProvider
         .state('home', {
             url: "/home",
-            templateUrl: "templates/home.html"
+            templateUrl: "templates/home.html",
+            controller: 'homeController'
         })
         .state('lection', {
             url: "/lection",
@@ -40,6 +41,21 @@ angular.module('weddingApp', ['ionic'])
 //             }
 //         });
 //  })
+.controller('homeController', ['$scope', '$ionicLoading', function ($scope, $ionicLoading) {
+    $ionicLoading.show({
+	    content: 'Loading Data',
+	    animation: 'fade-in',
+	    showBackdrop: false,
+	    maxWidth: 200,
+	    showDelay: 500
+	}); 
+
+    imagesLoaded( document.querySelector('body'), function( instance ) {
+        console.log('all images are loaded');
+        $ionicLoading.hide();
+    });
+}])
+
 .controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
     $scope.photos = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
     $scope.onImgClick = function(index) {
