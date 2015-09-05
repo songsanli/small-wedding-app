4518d91d1ac6735cb13ed9e574f6a83d554b3145
梁志艳 <liangzhiyan@kyee.com.cn>
2015-09-05 12:32:31 +0800

    photo延时加载（除了spinner的样式没有调整好）

--- www/js/app.js
+++ www/js/app.js
@@ -1,63 +1,49 @@
 angular.module('weddingApp', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('home', {
-            url: "/home",
-            templateUrl: "templates/home.html",
-            controller: 'homeController'
-        })
-        .state('lection', {
-            url: "/lection",
-            templateUrl: "templates/lection.html"
-        })
-        .state('timelocation', {
-            url: "/timelocation",
-            templateUrl: "templates/timelocation.html"
-        })
-        .state('thanks', {
-            url: "/thanks",
-            templateUrl: "templates/thanks.html"
-        })
-        .state('photos', {
-            url: "/photos",
-            templateUrl: "templates/photos.html",
-            controller: 'photoController'
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "templates/home.html",
+                controller: 'homeController'
+            })
+            .state('lection', {
+                url: "/lection",
+                templateUrl: "templates/lection.html"
+            })
+            .state('timelocation', {
+                url: "/timelocation",
+                templateUrl: "templates/timelocation.html"
+            })
+            .state('thanks', {
+                url: "/thanks",
+                templateUrl: "templates/thanks.html"
+            })
+            .state('photos', {
+                url: "/photos",
+                templateUrl: "templates/photos.html",
+                controller: 'photoController'
+            });
+
+        $urlRouterProvider.otherwise("/photos");
+    })
+    .controller('homeController', ['$scope', '$ionicLoading', function($scope, $ionicLoading) {
+        $scope.imagesLoaded = false;
+
+        $ionicLoading.show({
+            content: 'Loading Data',
+            animation: 'fade-in',
+            showBackdrop: false,
+            maxWidth: 200,
+            showDelay: 500
         });
 
-    $urlRouterProvider.otherwise("/home");
-})
-
-// I don't know whethe it's useful if I just need mobile web.
-// .run(function($ionicPlatform) {
-//         $ionicPlatform.ready(function() {
-//             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//             // for form inputs)
-//             if (window.cordova && window.cordova.plugins.Keyboard) {
-//                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//             }
-//             if (window.StatusBar) {
-//                 StatusBar.styleDefault();
-//             }
-//         });
-//  })
-.controller('homeController', ['$scope', '$ionicLoading', function ($scope, $ionicLoading) {
-    $scope.imagesLoaded = false;
-
-    $ionicLoading.show({
-	    content: 'Loading Data',
-	    animation: 'fade-in',
-	    showBackdrop: false,
-	    maxWidth: 200,
-	    showDelay: 500
-	}); 
-
-    imagesLoaded( document.querySelector('body'), function( instance ) {
-        console.log('all images are loaded');
-        $scope.imagesLoaded = true;
-        $ionicLoading.hide();
-    });
-}])
+        imagesLoaded(document.querySelector('body'), function(instance) {
+            console.log('all images are loaded');
+            $scope.imagesLoaded = true;
+            $ionicLoading.hide();
+        });
+    }])
 
 .controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
     $scope.photos = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
@@ -75,7 +61,34 @@ angular.module('weddingApp', ['ionic'])
         $scope.modal.hide();
     };
     $scope.$on('$destroy', function() {
-        $scope.modal&&$scope.modal.remove();
+        $scope.modal && $scope.modal.remove();
     });
 
+}])
+
+.directive('ionLazyImg', ['$http', '$timeout', '$compile', function($http, $timeout, $compile) {
+    return {
+        restrict:'E',
+        replace:true,
+        scope: {
+            imgUrl: '@',
+            imgClick: '&'
+        },
+        template: '<ion-spinner icon="ios"></ion-spinner>',
+        link: function($scope, $element, $attrs) {
+            $element.addClass('ion-lazy-img');
+            var eClass=$attrs['class'];
+            $element.find('svg').addClass(eClass);
+            var imgEl = $compile('<img src="{{imgUrl}}" class="ion-lazy-img"></img>')($scope);
+            imgEl.addClass(eClass);
+            imgEl.bind('click',function(e){
+                $scope.imgClick();
+            });
+            imgEl.bind('load',function(e){
+                $timeout(function(){
+                    $element.replaceWith(imgEl);
+                },0);
+            });
+        }
+    }
 }]);
