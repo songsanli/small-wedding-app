9b792e4ece9fd794d08a02f0dda251b88d34ae3f
Samuel Song <songsanli3@gmail.com>
2015-09-05 14:18:36 +0800

    调整缩进。

--- www/js/app.js
+++ www/js/app.js
@@ -1,49 +1,49 @@
 angular.module('weddingApp', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('home', {
-                url: "/home",
-                templateUrl: "templates/home.html",
-                controller: 'homeController'
-            })
-            .state('lection', {
-                url: "/lection",
-                templateUrl: "templates/lection.html"
-            })
-            .state('timelocation', {
-                url: "/timelocation",
-                templateUrl: "templates/timelocation.html"
-            })
-            .state('thanks', {
-                url: "/thanks",
-                templateUrl: "templates/thanks.html"
-            })
-            .state('photos', {
-                url: "/photos",
-                templateUrl: "templates/photos.html",
-                controller: 'photoController'
-            });
-
-        $urlRouterProvider.otherwise("/home");
-    })
-    .controller('homeController', ['$scope', '$ionicLoading', function($scope, $ionicLoading) {
-        $scope.imagesLoaded = false;
-
-        $ionicLoading.show({
-            content: 'Loading Data',
-            animation: 'fade-in',
-            showBackdrop: false,
-            maxWidth: 200,
-            showDelay: 500
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "templates/home.html",
+            controller: 'homeController'
+        })
+        .state('lection', {
+            url: "/lection",
+            templateUrl: "templates/lection.html"
+        })
+        .state('timelocation', {
+            url: "/timelocation",
+            templateUrl: "templates/timelocation.html"
+        })
+        .state('thanks', {
+            url: "/thanks",
+            templateUrl: "templates/thanks.html"
+        })
+        .state('photos', {
+            url: "/photos",
+            templateUrl: "templates/photos.html",
+            controller: 'photoController'
         });
 
-        imagesLoaded(document.querySelector('body'), function(instance) {
-            console.log('all images are loaded');
-            $scope.imagesLoaded = true;
-            $ionicLoading.hide();
-        });
-    }])
+    $urlRouterProvider.otherwise("/home");
+})
+.controller('homeController', ['$scope', '$ionicLoading', function($scope, $ionicLoading) {
+    $scope.imagesLoaded = false;
+
+    $ionicLoading.show({
+        content: 'Loading Data',
+        animation: 'fade-in',
+        showBackdrop: false,
+        maxWidth: 200,
+        showDelay: 500
+    });
+
+    imagesLoaded(document.querySelector('body'), function(instance) {
+        console.log('all images are loaded');
+        $scope.imagesLoaded = true;
+        $ionicLoading.hide();
+    });
+}])
 
 .controller('photoController', ['$scope', '$ionicModal', function($scope, $ionicModal) {
     $scope.photos = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27'];
